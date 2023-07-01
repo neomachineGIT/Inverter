c8bf51e6f3e607ee2900bec43d07767cf2d18bd8
neomachine <neomachinemail@gmail.com>
2023-07-01 10:59:57 -0500

    first commit

---
+++ ConsoleApp1/Listas.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    internal class Listas
+    {
+        public string[] Listado(string palab)
+        {
+            int cont = 0;
+            string[] lis = new string[palab.Count()];
+            foreach (char e in palab)
+            {
+                lis[cont] = e.ToString().ToLower();
+                cont++;
+            }
+            return lis;
+        }
+        public string[] Inverso(string[] lis)
+        {
+            int cont = 0;
+            string[] sil = new string[lis.Count()];
+            for (int i = lis.Count() - 1; i > -1; i--)
+            {
+                sil[cont] = lis[i].ToString().ToLower();
+                cont++;
+            }
+            Console.Write(" ");
+            foreach (string s in sil)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(s);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            Console.WriteLine("");
+            return sil;
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Listas ob = new Listas();
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("   DIGITE PALABRA A INVERTIR: \n--------------------------------");
+                Console.ForegroundColor= ConsoleColor.White;
+                string ss = Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine("--------------------------------");
+                Console.Write(" ");
+                foreach (string s in ob.Listado(ss))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(s);
+                    Console.ForegroundColor= ConsoleColor.White;
+                }
+                Console.WriteLine("\n");
+                ob.Inverso(ob.Listado(ss));
+                Console.WriteLine("\n--------------------------------");
+            }
+            
+        }
+    }
+}
